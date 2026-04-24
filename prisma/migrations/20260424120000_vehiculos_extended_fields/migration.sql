d58817a1a3b329b2edf3a531564b936c166acea0
eliascapasso <elias_capasso@live.com>
2026-04-24 15:36:34 -0300

    migración

---
+++ prisma/migrations/20260424120000_vehiculos_extended_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "vehiculos" ADD COLUMN "nroChasis" TEXT;
+ALTER TABLE "vehiculos" ADD COLUMN "poliza" TEXT;
+ALTER TABLE "vehiculos" ADD COLUMN "vencimientoPoliza" TIMESTAMP(3);
+ALTER TABLE "vehiculos" ADD COLUMN "tara" DOUBLE PRECISION;
+ALTER TABLE "vehiculos" ADD COLUMN "precinto" TEXT;

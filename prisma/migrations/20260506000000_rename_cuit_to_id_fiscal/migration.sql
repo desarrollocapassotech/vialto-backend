a14e5665543d238aee065ed2b38844ff251361a7
eliascapasso <elias_capasso@live.com>
2026-05-06 09:53:53 -0300

    id fiscal

--- prisma/migrations/20260506000000_rename_cuit_to_id_fiscal/migration.sql
+++ prisma/migrations/20260506000000_rename_cuit_to_id_fiscal/migration.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "tenants" RENAME COLUMN "cuit" TO "id_fiscal";
-ALTER TABLE "clientes" RENAME COLUMN "cuit" TO "id_fiscal";
-ALTER TABLE "transportistas" RENAME COLUMN "cuit" TO "id_fiscal";
+ALTER TABLE "tenants" RENAME COLUMN "cuit" TO "idFiscal";
+ALTER TABLE "clientes" RENAME COLUMN "cuit" TO "idFiscal";
+ALTER TABLE "transportistas" RENAME COLUMN "cuit" TO "idFiscal";
